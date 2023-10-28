ade8879a864a772c3bfd6351d231411436a5676d
dark-tracer <bernieamponsah2@gmail.com>
2023-10-27 22:24:52 -0700

    Task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - determines if a character is in lower case
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
