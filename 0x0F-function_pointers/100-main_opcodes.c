68483ca1ac6bd89cc7bdac0d60962b9ce353b2b9
dark-tracer <bernieamponsah2@gmail.com>
2023-11-08 07:56:28 -0800

    *a

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,20 +12,20 @@ int main(int argc, char *argv[])
 {
 	int bytes, i;
 	char *arr;
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	
+
 	bytes = atoi(argv[1]);
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	
+
 	arr = (char *)main;
 	for (i = 0; i < bytes; i++)
 	{
