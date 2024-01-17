88d0f894fc0b0078f56f5850e8565ab03682bfd0
dark-tracer <bernieamponsah2@gmail.com>
2024-01-17 02:27:59 -0800

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,23 +1,18 @@
 #include "main.h"
 #include <stdio.h>
-
-void _puts(char *str);
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
-int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}
-
 void _puts(char *str)
 {
-    /* Implementation of _puts function goes here */
-    /* For example: */
-    /* printf("%s\n", str); */
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
